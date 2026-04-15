1dddd4f876da14948fa82b5439ce6b37d542c8bb
Idwer <idwer@localhost>
2026-04-15 14:17:11 +0200

    src/routes/ape_post.rs: improve readability by not storing validated JSON data in a variable

--- src/routes/ape_post.rs
+++ src/routes/ape_post.rs
@@ -9,16 +9,14 @@ use crate::lib::ape::ApeIndexInput;
 
 #[handler]
 pub async fn ape_post(apeIndexInput: Json<ApeIndexInput>) -> Response {
-    let validated_input = apeIndexInput.validate();
-
-    match validated_input {
+    match apeIndexInput.validate() {
         Ok(_) => {
-            let input = ApeIndexInput {
+            let validated_input = ApeIndexInput {
                 height: apeIndexInput.height,
                 wingspan: apeIndexInput.wingspan,
             };
 
-            let output = input.ape_index_from_json();
+            let output = validated_input.ape_index_from_json();
             
             Json(output).into_response()
        }
